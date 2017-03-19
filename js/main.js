5d3d8d10dad01624fc4356b91ff7e39b00a2a7e6
Lisa Veras <lisaveras@gmail.com>
2017-03-19 07:17:58 -0700

    dont make a site without commiting again

---
+++ js/main.js
@@ -0,0 +1,58 @@
+function formatResponse(response){
+
+	$('#main').append('<div class="response"><b>Response:</b><br><br>'+ JSON.stringify(response.data) +'</div>');
+
+}
+
+function callDemo() {
+	axios.get('http://spotifyserver.herokuapp.com/api/people')
+	.then(function (response) {
+
+		$('#main').append('<div class="action"> Made a GET request to /api/people </div>');
+		formatResponse(response);
+
+	    return axios.post('http://spotifyserver.herokuapp.com/api/people', {
+	    	name: 'Sean',
+	    	favoriteCity: 'New York'
+	  })
+	})
+	.then(function (postedPerson) {
+		$('#main').append('<div class="action"> Made a Post request to /api/people, adding in Sean, whose favorite city is New York </div>');
+		formatResponse(postedPerson);
+
+		return axios.get('http://spotifyserver.herokuapp.com/api/people/'+postedPerson.data.id)
+	})
+	.then(function (postedPersonToUpdate){
+		$('#main').append('<div class="action"> Made a Get request to /api/people/${postedPersonToUpdate.id}');
+		formatResponse(postedPersonToUpdate);
+
+		return axios.put('http://spotifyserver.herokuapp.com/api/people/3', {
+			favoriteCity: 'Brooklyn'
+		})
+
+	})
+	.then(function(updatedPerson){
+		$('#main').append('<div class="action"> Made a Put request to /api/people/3, updating Sean\'s favorite city to Brooklyn.</div>');
+		formatResponse(updatedPerson);
+
+		return axios.get('http://spotifyserver.herokuapp.com/api/people/1');
+	})
+	.then(function(personOne){
+		$('#main').append('<div class="action"> Made a Get request to /api/people/1</div>');
+		formatResponse(personOne);
+
+		return axios.delete('http://spotifyserver.herokuapp.com/api/people/1');
+	})
+	.then(function(personDeleted){
+		$('#main').append('<div class="action"> Made a Delete request to /api/people/1</div>');
+		formatResponse(personDeleted);
+
+		return axios.get('http://spotifyserver.herokuapp.com/api/people');
+	})
+	.then(function(everyone){
+		$('#main').append('<div class="action"> Made a Get request to /api/people</div>');
+		formatResponse(everyone);
+	})
+}
+
+
