87ff18cba21c501d609327f2852b94ddb3bcb984
Lisa Veras <lisaveras@gmail.com>
2017-03-19 07:48:23 -0700

    cors

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ function formatResponse(response){
 }
 
 const options = {headers: {
-      'Access-Control-Allow-Origin': '*'
+      'Access-Control-Allow-Origin' : '*',
+      'Access-Control-Allow-Headers' : 'Origin, X-Requested-With, Content-Type, Accept'
   }}
 
 function callDemo() {
